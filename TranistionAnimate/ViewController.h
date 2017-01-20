dc3331cd0b3dd5870858ed471f71d5b6fb120d76
hzpqt <dahua@dahuadeMac-mini.local>
2017-01-20 13:44:16 +0800

    重新提交

---
+++ TranistionAnimate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransitionAnimate
+//
+//  Created by dahua on 17/1/20.
+//  Copyright © 2017年 dahua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TranistionAnimate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TransitionAnimate
+//
+//  Created by dahua on 17/1/20.
+//  Copyright © 2017年 dahua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
